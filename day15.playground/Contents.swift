3b4c77d4490410e20358cccf885573a2a629c226
am1006 <am1006@users.noreply.github.com>
2021-09-06 22:47:52 +1000

    day 15

---
+++ day15.playground/Contents.swift
@@ -0,0 +1,26 @@
+/**
+ # Swift review, day three
+ */
+
+/**
+ ## Properties
+ */
+
+/**
+ ## Static properties and methods
+ */
+
+/**
+ ## Access control
+ */
+
+/**
+ ## Polymorphism and typecasting
+ 
+ Note the **optional** in typecasting
+ */
+
+
+/**
+ ## Closures
+ */
