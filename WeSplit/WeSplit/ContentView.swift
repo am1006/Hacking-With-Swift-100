109ddbd9dd7d3227eae2e04430a69530a01b911e
am1006 <am1006@users.noreply.github.com>
2021-09-08 12:22:52 +1000

    day 17 - wesplit day 2

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,45 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    // @State allows us to work around the limitation of structs
-    // @State is specifically designed for simple properties that are stored in one view. As a result, Apple recommends we add private access control to those properties.
-    @State private var tapCount = 0
-    @State private var description = ""
-
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+    // Swift UI must use strings to store text field values
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2 // index
+    
+    @State private var tipPercentage = 2 // index
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        // calculate total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let orderAmount: Double = Double(checkAmount) ?? 0
+        return (orderAmount * (1 + tipSelection / 100)) / peopleCount
+    }
     
     var body: some View {
-        NavigationView {
+        NavigationView{
             Form {
-                // can only take up to 10 children!
-                // unless with a Group
-                // Or use Section
-                
                 Section {
-                    // We don't need two way binding here
-                    Text("Tap for \(description): \(tapCount)")
-                }
-
-                Section {
-                    // Two way binding here
-                    TextField("Enter the description: ", text: $description)
-                        .keyboardType(.twitter)
-                    Button("Tap to increase") {
-                        self.tapCount += 1
+                    TextField("Amount", text: $checkAmount)
+                        // modifier
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        // You need a NavigationView to navigate to the picker (sliding window view)
+                        ForEach(2..<100) {number in
+                            Text("\(number) People")
+                        }
+//                        .navigationBarTitle("Picker")
                     }
+                    .pickerStyle(WheelPickerStyle())
                 }
-
-                Section {
-                    Text("You chose: Student # \(students[selectedStudent])")
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(0 ..< students.count) {
-                            Text(self.students[$0])
+                
+                Section (header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {number in
+                            Text("\(self.tipPercentages[number])%")
                         }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("Tip Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
-            .navigationBarTitle(Text("💳 WeSplit"))
+            // It needs to be attached inside navigation view - we’re allowing iOS to change titles freely
+            .navigationBarTitle("💳 WeSplit")
         }
     }
 }
