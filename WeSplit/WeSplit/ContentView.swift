dcc1cab059cb88dfe77cba80cccd12270cdb2926
am1006 <am1006@users.noreply.github.com>
2021-09-09 22:15:43 +1000

    day 18 - wesplit day 3

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     // Swift UI must use strings to store text field values
     @State private var checkAmount = ""
+    @State private var numOfPeople = ""
     @State private var numberOfPeople = 2 // index
     
     @State private var tipPercentage = 2 // index
     let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var displayedNumOfPeople: String {
+        return String(numberOfPeople + 2)
+    }
     
     var totalPerPerson: Double {
         // calculate total per person here
@@ -22,6 +27,12 @@ struct ContentView: View {
         
         let orderAmount: Double = Double(checkAmount) ?? 0
         return (orderAmount * (1 + tipSelection / 100)) / peopleCount
+    }
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount: Double = Double(checkAmount) ?? 0
+        return (orderAmount * (1 + tipSelection / 100))
     }
     
     var body: some View {
@@ -37,9 +48,11 @@ struct ContentView: View {
                         ForEach(2..<100) {number in
                             Text("\(number) People")
                         }
-//                        .navigationBarTitle("Picker")
                     }
                     .pickerStyle(WheelPickerStyle())
+                    
+                    TextField("Number of people: \(displayedNumOfPeople)", text: $numOfPeople)
+                        .keyboardType(.numberPad)
                 }
                 
                 Section (header: Text("How much tip do you want to leave?")) {
@@ -49,6 +62,10 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("Total amount for the check")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
                 }
                 
                 Section (header: Text("Tip Per Person")) {
